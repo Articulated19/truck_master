ca5662f288f604b973cc76fe991fdb75125094a9
Lars Niklasson <larsniklasson94@gmail.com>
2019-02-19 14:31:50 +0100

    Clone file

---
+++ clone.sh
@@ -0,0 +1,24 @@
+git clone https://github.com/Articulated19/path_planning
+git clone https://github.com/Articulated19/truck_map
+git clone https://github.com/Articulated19/multi_planner
+git clone https://github.com/Articulated19/section-locks-visualizer
+git clone https://github.com/Articulated19/section_identifier
+git clone https://github.com/Articulated19/section_lock
+git clone https://github.com/Articulated19/multi-planner-socket
+git clone https://github.com/Articulated19/truck_automatic_ctrl
+git clone https://github.com/Articulated19/ultra-vision_18
+git clone https://github.com/Articulated19/zookeeper
+git clone https://github.com/Articulated19/custom_msgs
+git clone https://github.com/Articulated19/gv_socket_server
+git clone https://github.com/Articulated19/truck_master
+git clone https://github.com/Articulated19/gulliview_server
+git clone https://github.com/Articulated19/truck_visualization
+git clone https://github.com/Articulated19/intersection-simulator
+git clone https://github.com/Articulated19/documentation
+git clone https://github.com/Articulated19/vision_localization_18
+git clone https://github.com/Articulated19/truck_manual_ctrl
+git clone https://github.com/Articulated19/truck_hw_api
+git clone https://github.com/Articulated19/position_estimation
+git clone https://github.com/Articulated19/mapserver
+git clone https://github.com/Articulated19/extended_kalman
+
